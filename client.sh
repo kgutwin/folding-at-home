0b4654cc1828de85468f1f86bcacc052a719e781
Karl Gutwin <karl@bioteam.net>
2020-03-23 16:32:38 -0400

    Initial CFN template for spot fleet

--- client.sh
+++ client.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+FAHUSER=${1:-karlgutwin}
+FAHTEAM=${2:-245750}
+
 yum -y install https://download.foldingathome.org/releases/public/release/fahclient/centos-6.7-64bit/v7.5/fahclient-7.5.1-1.x86_64.rpm
 
 cat > /etc/fahclient/config.xml <<EOF 
-<config><power v='full'/><user v='karlgutwin'/><team v='245750'/><gpu v='true'/>
+<config><power v='full'/><user v='$FAHUSER'/><team v='$FAHTEAM'/><gpu v='true'/>
 </config>
 EOF
 service FAHClient restart
